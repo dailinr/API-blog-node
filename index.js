7167f77197a6fa94f4a31dc1eeb5dca30bb9dcfd
Dailin Romero <dailinromero123@gmail.com>
2024-06-27 22:35:40 -0500

    crear servidor

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { conexion } = require("./database/conexion"); // importar archivo de conexion
 const express = require("express"); // importo el paq express de mis dependencias
+const cors = require("cors");
 
 console.log("App node arrancada");
 
@@ -9,9 +10,17 @@ conexion();
 
 // Crear servidor Node
 const app = express();
+const puerto = 3900;
 
 // Configurar cors
 app.use(cors()); // se ejecuta el cors antes de que se ejecute cualquier ruta
 
 // Convertir body a objeto js
-app.use(express)
+app.use(express.json());  // parsea automaticamente los datos enviados por POST a objetos javascript
+
+// Crear Rutas
+
+// Crear servidor y escuchar peticiones http
+app.listen(puerto, () => { // se pasa un puerto como parametro y funcion verifique q el servidor corra
+    console.log("Servidor corriendo en el puerto: "+puerto);
+});
