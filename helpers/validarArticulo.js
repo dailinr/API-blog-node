884f6d677cdcebe453fe0e01de938d2c9260ac01
Dailin Romero <dailinromero123@gmail.com>
2024-08-14 18:20:00 -0500

    validar etiqueta

--- helpers/validarArticulo.js
+++ helpers/validarArticulo.js
@@ -6,10 +6,11 @@ const validarArticulo = (parametros) => {
     
     let valLongTitulo = validator.isLength(parametros.titulo, {min: 5, max: undefined}); // valiudamos que tenga una longitud entre 5 - sin limite
     let validarTitulo = !validator.isEmpty(parametros.titulo) && valLongTitulo; // validar si el titulo no está vacio
-                            
+    
+    let validarEtiqueta = !validator.isEmpty(parametros.etiqueta);     
     let validarContenido = !validator.isEmpty(parametros.contenido);
 
-    if(!validarTitulo || !validarContenido){
+    if(!validarTitulo || !validarContenido || !validarEtiqueta){
         throw new Error("No se ha validado la información !!"); // se lanzaria una excepción
     }
     
